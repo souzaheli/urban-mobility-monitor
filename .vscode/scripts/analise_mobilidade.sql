d503f830cf12e0c98e51b6534a25469a5cc29550
Heli de Souza Oliveira <Heli de Souza>
2026-01-07 22:14:38 -0400

    adiciona métrica de estimativa de CO1 por bairro

--- .vscode/scripts/analise_mobilidade.sql
+++ .vscode/scripts/analise_mobilidade.sql
@@ -2,5 +2,6 @@
 SELECT bairro,
     fluxo_veiculos,
     etr_indice -- fonte dos dados
+    (fluxo_veiculos * 0.12) AS estimativa_co2_kg
 FROM trafego_campo_grande -- apenas o que é "Crítico": Bairros com entropia acima de 0.80
-WHERE etr_indice > 0.80;
+WHERE etr_indice > 0.70;
