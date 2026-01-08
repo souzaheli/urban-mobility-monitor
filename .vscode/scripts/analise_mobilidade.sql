a10b4180fd689b81bf1fbccb7a3d46047c514e71
Heli de Souza Oliveira <Heli de Souza>
2026-01-07 21:51:13 -0400

    feat: cria primeira query SQL para filtragem de entropia urbana

---
+++ .vscode/scripts/analise_mobilidade.sql
@@ -0,0 +1,6 @@
+-- colunas que nos interessam para a análise de Smart City
+SELECT bairro,
+    fluxo_veiculos,
+    etr_indice -- fonte dos dados
+FROM trafego_campo_grande -- apenas o que é "Crítico": Bairros com entropia acima de 0.80
+WHERE etr_indice > 0.80;
